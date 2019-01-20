80793d1bb7631093a844d0f5f5c1d397ff2e36c2
yusefrich <yusef.rick@gmail.com>
2019-01-20 16:32:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+
+# Back end python and psql
+
+Source code for a backend server made with python
+and psql, this is a repo for studies and consultation purpose
+
+## Getting Started
+
+Install Vagrant And VirtualBox and
+clone this repositore and log into the vagrant machine
+### Prerequisites
+
+* Python 3
+
+```
+donwload on python.org
+```
+
+* Vagrant
+
+```
+donwload on vagrantup.com/downloads.html
+```
+* Virtual box
+
+```
+donwload on virtualbox.org/wiki/Downloads
+```
+### Installing
+
+* Run vagrant up to start the vm with the virtual box
+
+```
+vagrant up
+```
+
+* Log on the vagrant virtual machine
+
+```
+vagrant ssh
+```
+
+
+
+## Authors
+
+* **udacity team** - *all work* - check on the CODEOWNERS file
+
+
+# Udacity student data
+
+ *NAME*: Yusef Richard de Oliveira Alves <p>
+ *COURSE*: udacity Nanodegree fullstack <p>
+ *EMAIL*:yusef.rick@gmail.com <p>
---
+++ vagrant/catalog/views.py
@@ -0,0 +1,72 @@
+from redis import Redis
+import time
+from functools import update_wrapper
+from flask import request, g
+from flask import Flask, jsonify, render_template
+from redis import Redis
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+import json
+
+app = Flask(__name__)
+
+# fake database
+fcategory = {'name': 'soccer', 'id': '1'}
+fcategories = [{'name': 'soccer', 'id': '1'}, {'name': 'Basketball', 'id': '2'}, {'name': 'Baseball', 'id': '3'}]
+fitem = {'name': 'ball', 'description': 'this is a ball lul', 'id': '1'}
+fitems = [{'name': 'ball', 'description': 'this is a ball lul', 'id': '1'},
+         {'name': 'goggles', 'description': 'this is a goggle lul lul', 'id': '2'},
+         {'name': 'snowboard', 'description': 'this is a snowboard lul lul lul', 'id': '3'}]
+
+
+
+@app.route('/')
+@app.route('/catalog')
+def showCatalogs():
+    return render_template('all_catalog.html', categories=fcategories)
+
+
+# leads to edit item if logged
+# leads to delete item if logged
+@app.route('/catalog/<path:category>/items')
+def showCategoryItems(category):
+    return render_template('show_category_items.html', categories=fcategories, current_category=category, items=fitems)
+
+
+# leads to edit item if logged
+# leads to delete item if logged
+@app.route('/catalog/<path:category>/<path:item>')
+def showItem(category, item):
+    return render_template('show_item.html', categories=fcategories, current_item=item, item=fitem)
+
+
+# needs to be loged
+@app.route('/catalog/<path:item>/edit')
+def editItem(item):
+    return render_template('edit_item.html', categories=fcategories, current_item=item, item=fitem)
+
+
+# needs to be loged
+@app.route('/catalog/<path:item>/delete')
+def deleteItem(item):
+    return render_template('delete_item.html', categories=fcategories, current_item=item, item=fitem)
+
+
+# needs to be loged
+@app.route('/catalog/add')
+def addItem():
+    return render_template('add_item.html', categories=fcategories)
+
+
+@app.route('/catalog.json')
+def showJson():
+    return "this is the json of all the catalog items"
+
+
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
